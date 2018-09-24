5b38c44450e22ca89869a76061c41017b151fa8b
scogliani <simon.cogliani@gmail.com>
2018-09-24 16:05:09 +0200

    Add skeleton file shell

---
+++ .vim/templates/skeleton.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+set -o xtrace
+
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)"
+
+arg1="${1:-}"
