b718cc3424057eaa688a27dc126c2b16b7d21bd1
scogliani <simon.cogliani@gmail.com>
2021-03-23 00:25:38 +0100

    new python template

--- .vim/templates/skeleton.py
+++ .vim/templates/skeleton.py
@@ -1,15 +1,17 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
 import sys
 
-def main():
+
+def main(*args, **kwargs):
     return 0
 
 
 if __name__ == "__main__":
     try:
         sys.exit(main())
-    except Exception as e:
-        print(e)
+    except Exception:
+        import traceback as tb
+        print(tb.format_exc(), file=sys.stderr)
         raise SystemExit(1)
